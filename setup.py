70bc2d13e9283529e048eaec718475dab52b031d
Charles Bernard <bernardcharles@gmail.com>
2020-06-10 13:19:09 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lendingclub_scoring',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict if the loans in our dataset are going to be fully repaid',
+    author='Charles Bernard',
+    license='',
+)
